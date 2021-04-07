d81463d1612ab157c0e77ecef2dc18caaed84c66
Pat Rivet <patrickmrivet@gmail.com>
2021-04-07 14:43:21 +0100

    Add fetch to get public photos from Flickr API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import JSONP from "jsonp";
+
+// Custom components
+import PhotoList from './components/PhotoList/PhotoList';
+
+const BASE_URL = 'https://api.flickr.com/services/feeds/photos_public.gne?&format=json&lang=en-us&tags=safe';
+const HEADERS = {
+  "param": "jsoncallback"
+}
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    JSONP(BASE_URL, HEADERS, (err, data) => {
+      if (err) {
+        console.log(`ERROR occured fetching url =${BASE_URL}. Error =`);
+        console.log(err);
+      } else {
+        setPhotos(data.items);
+        setReady(true);
+      }
+    });
+
+  }, []);
+
   return (
     <div className="App">
+      {ready && <PhotoList photos={photos}/>}
+      {!ready && <div>Loading...</div>}
     </div>
   );
 }
